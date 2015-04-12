29b7d9b8e73f33f033b4468a901f5273d1618120
arun843p <arun843p@gmail.com>
2015-04-12 17:45:44 -0400

    Third & Fourth Plots
    Third & Fourth Plots

--- plot4.R
+++ plot4.R
@@ -1,34 +1,42 @@
-plot3 <- function() {
+plot4 <- function() {
         ##Get Required data for charting
         df <- read_req_data()
         
         ##Convert Required Chart elements to Numeric class
+        df$Global_active_power <- as.numeric(df$Global_active_power)
+        df$Voltage <- as.numeric(df$Voltage)
+        df$Global_reactive_power <- as.numeric(df$Global_reactive_power)
         df$Sub_metering_1 <- as.numeric(df$Sub_metering_1)
         df$Sub_metering_2 <- as.numeric(df$Sub_metering_2)
         df$Sub_metering_3 <- as.numeric(df$Sub_metering_3)
         
         ##Open & Create Blank png file
-        png(file= "plot3.png")
+        png(file= "plot4.png")
         
-        ##Blank Plot
+        ##Setting Plotting area for 4 plots
+        par(mfrow=c(2,2),mar=c(4,4,2,1),oma=c(2,0,0,1))
+        
+        ##1st Plot
+        with(df, plot(y= Global_active_power,x=Time, type = "l", ylab = "Gloabl Active Power",xlab=""))
+        
+        ##2nd Plot
+        with(a1, plot(y= Voltage,x=Time, type = "l", ylab = "Voltage",xlab="datetime"))
+        
+        ##3rd Plot
         with(df, plot(y= Sub_metering_1,x=Time, type = "n", ylab = "",xlab=""))
-        
-        ##Plotting 3 Sub Metering Readings
-        ##Send Chart on RStudio Graphic Devic
         with(df, lines(y= Sub_metering_1,x=Time, lty=1,lwd=1, ylab = "",xlab=""))
-        
         with(df, lines(y= Sub_metering_2,x=Time, lty=1, lwd=1, ylab = "",xlab="",col = "red"))
-        
         with(df, lines(y= Sub_metering_3,x=Time, lty=1, lwd=1, ylab = "",xlab="",col = "blue"))   
-        
         ##Legends
-        legend("topright", pch=c(NA,NA,NA), col = c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lwd=1)
-        
+        legend("topright",bty="n", pch=c(NA,NA,NA), col = c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lwd=1, cex=0.8)
         ##Label
         title(ylab="Energy Sub Metering")
         
+        ##4th Plot
+        with(df, plot(y= Global_reactive_power,x=Time, type = "l", xlab = "datetime"))
+        
         ##Close PNG file
-        dev.off()        
+        dev.off()
 }
 
 read_req_data <- function(){
